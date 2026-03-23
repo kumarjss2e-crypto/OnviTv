6f2a6786dfb53880424fd3ebc25e8a59421de8c4
OnviTV Developer <kumarjss2e@gmail.com>
2026-03-23 14:23:02 +0100

    Improve: Add explicit project-level deployment target override and save Xcode project

--- ios/Podfile.post_install.rb
+++ ios/Podfile.post_install.rb
@@ -1,21 +1,20 @@
-post_install do |installer|  
-  # Workaround: Remove platform from deployment target settings
-  # This allows Xcode's range enforcement (12.0-26.2.99) to apply
+post_install do |installer|
+  require 'xcodeproj'
+  
+  # Set all targets to iOS 13.0 deployment target
   installer.pods_project.targets.each do |target|
     target.build_configurations.each do |config|
-      # Explicitly set to 13.0 for all pods
+      # Force deployment target to 13.0
       config.build_settings['IPHONEOS_DEPLOYMENT_TARGET'] = '13.0'
-      
-      # Remove any iOS-specific overrides that might conflict
-      if config.build_settings['OTHER_CFLAGS']
-        flags = config.build_settings['OTHER_CFLAGS']
-        flags = flags.delete_if { |flag| flag.include?('min-version') } if flags.is_a?(Array)
-      end
+      config.build_settings['SWIFT_VERSION'] = '5.0'
     end
   end
   
-  # Fix the project-level deployment target
+  # Also set at project level
   installer.pods_project.build_configurations.each do |config|
     config.build_settings['IPHONEOS_DEPLOYMENT_TARGET'] = '13.0'
   end
+  
+  # Save the modified project
+  installer.pods_project.save
 end
